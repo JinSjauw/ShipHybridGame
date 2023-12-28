aac9546d1923e25bc141aa0e6b70d346c14b296e
JinSjauw <jinzhouliu4@gmail.com>
2023-12-28 23:13:04 +0100

    Reworked the physics (works more like a car now..)

--- Assets/BoatController.cs
+++ Assets/BoatController.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody))]
 public class BoatController : MonoBehaviour
 {
-    [SerializeField] private List<Floater> _floaters;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private Transform rudderTransform;
+    [FormerlySerializedAs("enginePosition")]
+    [Header("Boat References")] 
+    [SerializeField] private Transform engineTransform;
+    [SerializeField] private List<Transform> steeringPoints;
+    [SerializeField] private List<Floater> drivingPoints;
 
+    [Header("Boat Movement Variables")]
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
     [SerializeField] private float maxTurnAngle = 40;
     
     private Rigidbody boatBody;
+
+    private bool isMoving;
+    private bool isTurning;
+    
     private float throttle;
     private float turnRate;
     private float turnAngle;
@@ -27,31 +36,38 @@ public class BoatController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        
         if (throttle != 0 && WaterCheck())
         {
             Vector3 velocity = throttle * boatBody.transform.forward * moveSpeed * Time.deltaTime;
-            boatBody.transform.Rotate(Vector3.up, turnRate);
+            //boatBody.transform.Rotate(Vector3.up, turnRate * turnSpeed);
+            //boatBody.AddTorque(boatBody.transform.up * (turnRate * turnSpeed), ForceMode.VelocityChange);
             //velocity = Quaternion.AngleAxis(turnAngle, Vector3.up) * velocity;
-            boatBody.AddForce(velocity, ForceMode.VelocityChange);
+
+            boatBody.AddForce(velocity, ForceMode.Acceleration);
+        }
+
+        if (isTurning)
+        {
+            foreach (Transform point in steeringPoints)
+            {
+                Debug.Log(turnAngle);
+                if (Mathf.Abs(turnAngle + turnRate) < maxTurnAngle)
+                {
+                    float angleChange = turnRate * turnSpeed * Time.fixedDeltaTime;
+                    turnAngle += angleChange;
+                    turnAngle = Mathf.Clamp(turnAngle, -maxTurnAngle, maxTurnAngle);
+                    //point.rotation *= Quaternion.AngleAxis(angleChange, Vector3.up);
+                    point.localRotation = Quaternion.Euler(0, turnAngle, 0);
+                }
+            }
         }
         
-        //Rotate Rudder (if turnangle changed)
-        /*if (turnRate != 0)
-        {
-            if (Mathf.Abs(turnAngle) < maxTurnAngle)
-            {
-                Debug.Log("Changing Direction:" + turnRate + " : " + turnAngle);
-                turnAngle += turnRate * turnSpeed * Time.deltaTime;
-            }
-            rudderTransform.localRotation = Quaternion.AngleAxis(turnAngle, Vector3.up);
-        }*/
     }
 
     private bool WaterCheck()
     {
-        if (Physics.Raycast(boatBody.position, Vector3.down, 10f, LayerMask.GetMask("Water")))
+        Vector3 enginePosition = engineTransform.position;
+        if (WaveManager.Instance.FindPoint(new Vector2(enginePosition.x, enginePosition.z)).y > enginePosition.y)
         {
             return true;
         }
@@ -63,15 +79,22 @@ public class BoatController : MonoBehaviour
     {
         if (context.performed)
         {
-            Debug.Log("Moving: " + context.ReadValue<Vector2>());
+            //Debug.Log("Moving: " + context.ReadValue<Vector2>());
             throttle = context.ReadValue<Vector2>().y;
             turnRate = context.ReadValue<Vector2>().x;
+
+            if (turnRate != 0)
+            {
+                isTurning = true;
+            }
             //boatBody.AddForce(context.ReadValue<float>() * boatBody.transform.right * moveSpeed, ForceMode.Acceleration);
         }
 
         if (context.canceled)
         {
             throttle = 0;
+            turnRate = 0;
+            isTurning = false;
         }
     }
 }
--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -1,27 +1,59 @@
 using System;
+using TreeEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Floater : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Rigidbody boatBody;
     [SerializeField] float depthBeforeSubmerged = 1f;
     [SerializeField] float displacementAmount = 3f;
     [SerializeField] private int floaterCount = 1;
     [SerializeField] private float waterDrag = 0.99f;
     [SerializeField] private float waterAngularDrag = 0.5f;
+    [SerializeField] [Range(0f, 0.5f)]private float floaterMass = .2f;
     
     private void FixedUpdate()
     {
-        rigidbody.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+        Vector3 floaterPosition = transform.position;
         
+        boatBody.AddForceAtPosition(Physics.gravity / floaterCount, floaterPosition, ForceMode.Acceleration);
         //find point
-        float waveHeight = WaveManager.Instance.FindPoint(new Vector2(transform.position.x, transform.position.z)).y;
-        if (transform.position.y < waveHeight)
+        float waveHeight = WaveManager.Instance.FindPoint(new Vector2(floaterPosition.x, floaterPosition.z)).y;
+        if (floaterPosition.y < waveHeight)
         {
-            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
-            rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
-            rigidbody.AddForce(displacementMultiplier * -rigidbody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
-            rigidbody.AddTorque(displacementMultiplier * -rigidbody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            /*float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            //Pushes the floating point above waveheight
+            boatBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            //Need to look into this part so the boat can glide through the waves when moving
+            //Adds drag to the floating point //Maybe less drag when moving?
+            boatBody.AddForce(displacementMultiplier * (-boatBody.velocity * waterDrag) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            boatBody.AddTorque(displacementMultiplier * (-boatBody.angularVelocity * waterAngularDrag) * Time.fixedDeltaTime, ForceMode.VelocityChange);*/
+            
+            //Bouyancy - Y Axis
+            //Make it so the boat tries to get to the wave height and then stops 
+            Vector3 surfaceDirection = transform.up;
+
+            Vector3 floaterWorldVelocity = boatBody.GetPointVelocity(floaterPosition);
+            
+            //Calculate the offset (distance between waveHeight and floater position)
+            float offset = Mathf.Clamp01(waveHeight - floaterPosition.y);
+            
+            //Calculate velocity along the up axis of the floater
+            float velocity = Vector3.Dot(surfaceDirection, floaterWorldVelocity);
+
+            float force = (offset * displacementAmount) - (velocity * waterDrag);
+            
+            boatBody.AddForceAtPosition(surfaceDirection * force, floaterPosition);
+            
+            //Bouyancy - Drag/Steering
+            Vector3 steeringDirection = transform.right;
+            float steeringVelocity = Vector3.Dot(steeringDirection, floaterWorldVelocity);
+            float desiredVelocityChange = -steeringVelocity * waterDrag;
+            float desiredAcceleration = desiredVelocityChange / Time.fixedDeltaTime;
+            
+            //Apply drag force
+            boatBody.AddForceAtPosition(steeringDirection * floaterMass * desiredAcceleration, floaterPosition);
         }
     }
 }
