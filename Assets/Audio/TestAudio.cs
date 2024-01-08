212ea86e714962a9decdd0cd90ec268275081592
JinSjauw <jinzhouliu4@gmail.com>
2024-01-08 22:59:13 +0100

    Audio Manager added

--- Assets/Audio/TestAudio.cs
+++ Assets/Audio/TestAudio.cs
@@ -1,9 +1,31 @@
 
+using System.Diagnostics;
+using Audio;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TestAudio : MonoBehaviour
 {
     public AudioController audioController;
-    
-    
+
+    public void OnPlaySound(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            switch (context.ReadValue<float>())
+            {
+                case 1:
+                    audioController.PlayAudio(AudioID.ST_MUSIC, false, 1.0f);
+                    break;
+                case -1:
+                    audioController.RestartAudio(AudioID.ST_MUSIC, true, .5f);
+                    break;
+            }
+        }
+
+        if (context.canceled)
+        {
+            audioController.StopAudio(AudioID.ST_MUSIC, false, 0);
+        }
+    }
 }
