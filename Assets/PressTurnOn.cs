70c992b39777581d5e9f47eb8230c14a9b256628
LeviSulmann <levi.sulmann@student.hku.nl>
2024-01-15 18:21:39 +0100

    AudioUpdate

--- Assets/PressTurnOn.cs
+++ Assets/PressTurnOn.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PressTurnOn : MonoBehaviour
 {
+    public GameObject Audio1;
+    bool Klopt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Audio1.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Klopt = true;
+        }
+       
+
+        if(Klopt == true)
+        {
+            Audio1.SetActive(true);
+        }
+        else
+        {
+            Audio1.SetActive(false);
+        }
     }
 }
