e49ca9ae51bd01996548cc42ed41b611c5bbf67f
LeviSulmann <levi.sulmann@student.hku.nl>
2024-01-16 13:56:38 +0100

    MoreFish
    yes

--- Assets/Fish.cs
+++ Assets/Fish.cs
@@ -9,6 +9,8 @@ public class Fish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        Fishcount = GameObject.FindWithTag("AllFish");
      // Fishcount.GetComponent<FishSound>();
     }
 
